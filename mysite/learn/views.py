192888fcb66a274c0175a55a223d19541427c86d
qianjunye <qianjunye@meituan.com>
2015-06-26 11:22:03 +0800

    mysite django web

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -1,6 +1,5 @@
 #coding:utf-8
-from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request)
+def index(request):
 	return HttpResponse(u"welcome Junye")
